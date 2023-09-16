241a9b41dba86d816f580d3e3f8efc1e5d702101
Dayvid0063 <orjidavid0063@gmail.com>
2023-09-16 10:29:44 -0700

    get_size.c

---
+++ get_size.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * get_size - Func prints size
+ * @format: Fmtted str
+ * @i: List arguments
+ *
+ * Return: The size
+ */
+int get_size(const char *format, int *i)
+{
+int r = *i + 1;
+int k = 0;
+if (format[r] == 'l')
+k = S_LONG;
+else if (format[r] == 'h')
+k = S_SHORT;
+if (k == 0)
+*i = r - 1;
+else
+*i = r;
+return (k);
+}
