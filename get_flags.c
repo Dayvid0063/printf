d39908b067583fc62fbe932a227eed87afe4e913
Dayvid0063 <orjidavid0063@gmail.com>
2023-09-17 08:21:23 -0700

    flagupdate

--- get_flags.c
+++ get_flags.c
@@ -9,21 +9,29 @@ int get_flags(const char *format, int *i)
 {
 /* - + 0 # ' ' */
 /* 1 2 4 8  16 */
-int o, r;
+int o, curr_i;
 int flags = 0;
 const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
-for (r = *i + 1; format[r] != '\0'; r++)
+
+for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 {
 for (o = 0; FLAGS_CH[o] != '\0'; o++)
-if (format[r] == FLAGS_CH[o])
+{
+if (format[curr_i] == FLAGS_CH[o])
 {
 flags |= FLAGS_ARR[o];
 break;
 }
+}
+
 if (FLAGS_CH[o] == 0)
+{
 break;
 }
-*i = r - 1;
+}
+
+*i = curr_i - 1;
+
 return (flags);
 }
