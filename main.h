d249e7390fdda6b3908bed3d9d137ccbf5d79f3b
Dayvid0063 <orjidavid0063@gmail.com>
2023-09-19 10:11:46 -0700

    a.outupdate

--- main.h
+++ main.h
@@ -21,8 +21,8 @@
 /**
  * struct fmt - Struct op
  *
- * @fmt: The format.
- * @fn: The function associated.
+ * @fmt: The fmt.
+ * @fn: The func.
  */
 struct fmt
 {
@@ -34,8 +34,8 @@ struct fmt
 /**
  * typedef struct fmt fmt_t - Struct op
  *
- * @fmt: The format.
- * @fm_t: The function associated.
+ * @fmt: The fmt.
+ * @fm_t: The func.
  */
 typedef struct fmt fmt_t;
 
